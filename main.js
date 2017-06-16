3bbb890081507817e945745ba9ebe81e6cd589ad
joshuahunt10 <joshua.hunt88@gmail.com>
2017-06-16 10:56:58 -0500

    track search complete

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 //current progress: Can search for Thrice and click the Thrice word and it shows a song of theirs.
-// TODO: be able to pass in a parameter to artistSongs. This will make it dynamic.  Also make the div clear so only the songs show.
 
 
 const token = '?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f';
@@ -18,34 +17,28 @@ function findArtist(userSearch){
       let bandID = json[0].id;
 
       let htmlForm = `
-        <div class="band-art">
-          <img src="${bandAvatar}">
-        </div>
-        <div class="band-name">
-          <p class="band-name-p">${bandName}</p>
+        <div class="band-wrapper"
+          <div class="band-art">
+            <img src="${bandAvatar}">
+          </div>
+          <div class="band-name">
+            <p class="band-name-p">${bandName}</p>
+          </div>
         </div>
       `
-
-{/* <p><a class="band-name-link" href = "https://api.soundcloud.com/users/${bandID}/tracks/?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f&q=${userSearch}">${bandName}</p></a> */}
-
 
       document.querySelector('.search-results').textContent = "";
       document.querySelector('.search-results').insertAdjacentHTML('afterbegin', htmlForm);
 
-      document.querySelector('.band-name').addEventListener('click', artistSongs)
-      // document.querySelector('.player').insertAdjacentHTML('afterbegin', htmlAudio);
-
+      document.querySelector('.band-wrapper').addEventListener('click', function(){
+        artistSongs(bandID, bandName)
+      })
     })
-
 }
 
-// document.querySelector('.band-name-link').addEventListener('click', function artistSongs(){
-//   console.log('event listener added')
-// })
-
-function artistSongs(){
-  // document.querySelector('.search-results').textContent = "Searching for... " + userSearch;
-  fetch('https://api.soundcloud.com/users/211099306/tracks/?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f&q=thrice', {
+function artistSongs(bandID, searchName){
+  document.querySelector('.search-results').textContent = "Fetching songs for..." + searchName;
+  fetch('https://api.soundcloud.com/users/' + bandID +'/tracks/?client_id=095fe1dcd09eb3d0e1d3d89c76f5618f&q=' + searchName, {
   })
     .then(function (response) {
       return response.json()
@@ -58,17 +51,24 @@ function artistSongs(){
       let songLink = json[0].stream_url;
 
       let htmlSongs = `
-      <div class="band-art">
-        <img src="${albumArt}">
-      </div>
-      <div class="band-name">
-        <p>${songTitle}</p>
-      </div>
+        <div class='band-wrapper'
+          <div class="band-art">
+            <img src="${albumArt}">
+          </div>
+          <div class="band-name">
+            <p>${songTitle}</p>
+          </div>
+        </div>
       `
-
+      document.querySelector('.search-results').textContent = ""
       document.querySelector('.search-results').insertAdjacentHTML('afterbegin', htmlSongs);
-
   })
+}
+
+document.querySelector('.reset-button').addEventListener('click', resetButton)
+
+function resetButton(){
+  document.querySelector('.search-results').textContent = ""
 }
 
 document.querySelector('.search-form').addEventListener('submit', function(event){
